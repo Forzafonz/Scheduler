e68f7fdabbdcc9eb58e34586d14c44d4dfe396d9
Anton <golubin1990@gmail.com>
2021-08-31 21:29:16 -0400

    Small formatting fixes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,22 +1,15 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-
-   let buttonClass = "button";
-
-   if (props.confirm) {
-     buttonClass += " button--confirm";
-   } 
-   
-   if (props.danger) {
-      buttonClass += " button--danger";
-   }
+   const buttonClass = classNames('button', {"button--confirm" : props.confirm}, {"button--danger": props.danger});
+   console.log(buttonClass)
 
    return (<>
       <button 
-         class = {buttonClass} 
+         className = {buttonClass} 
          onClick = {props.onClick}
          disabled = {props.disabled}>
          {props.children}
