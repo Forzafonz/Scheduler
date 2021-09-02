db5ba2e5faa3d5a28159ead43e1abb064746073e
Anton <golubin1990@gmail.com>
2021-09-02 10:02:46 -0400

    Refactor to have controlled componenet

--- src/components/Interviewers/InterviewerListItem.jsx
+++ src/components/Interviewers/InterviewerListItem.jsx
@@ -11,20 +11,20 @@ import classNames from 'classnames';
 
 
 export default function InterviewerListItem(props) {
-  const {name, avatar, selected, setInterviewer} = props;
+  const {name, avatar, selected, onChange} = props;
   const itemClass = classNames("interviewers__item", {"interviewers__item--selected" : selected})
   
   return (
     <li className = {itemClass} 
-    onClick = {setInterviewer} 
+    onClick = {onChange} 
     {...selected}>
     <img
       className="interviewers__item-image"
       src={avatar}
       alt="{name}"
     />
-    {selected && name};
+    {selected && name}
   </li>
-  );
+  )
 
 };
