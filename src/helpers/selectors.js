de987169e58cd505608905ef73d0234aefeca8fb
Anton <golubin1990@gmail.com>
2021-09-03 20:08:20 -0400

    Add a new selector for interview information and tests for it

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,3 +1,4 @@
+
 export function getAppointmentsForDay(state, day) {
   const {days, appointments } = state;
   if (!days.length) return [];
@@ -5,3 +6,19 @@ export function getAppointmentsForDay(state, day) {
   if (!selectedDayAppointments.length) return [];
   return selectedDayAppointments[0].appointments.map(id => appointments[id]);
 }
+
+
+/**
+ * @param {object} state - state object, from which only interviewers is required: {"1": {  "id": 1,"name": "Sylvia Palmer","avatar": "https://i.imgur.com/LpaY82x.png" }
+ * @param {object} interview - interview object like: { student: "Lydia Miller-Jones", interviewer: 1 }
+ * @returns {object} - object like {  "student": "Lydia Miller-Jones", "interviewer": {  "id": 1, "name": "Sylvia Palmer", "avatar": "https://i.imgur.com/LpaY82x.png"} }
+ */
+
+export function getInterview(state, interview) {
+  const { interviewers } = state;
+  if (interview === null) return null;
+  const interviewerId = interview.interviewer;
+  const interviewerDetails = interviewers[interviewerId];
+  return Object.assign({}, {student: interview.student}, {interviewer : interviewerDetails});
+}
+
