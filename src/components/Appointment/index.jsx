da0416aeee39dacbf83edc5dd2727db16c23eee9
Anton <golubin1990@gmail.com>
2021-09-04 20:42:13 -0400

    Change rendering for Appointment component

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,20 +1,31 @@
 
-import React, {Fragment} from 'react';
+import React, {Fragment, useEffect} from 'react';
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import Form from './Form/Form';
 import './styles.scss';
+import useVisualMode from 'hooks/useVisualMode';
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE"
+
 
 
 export default function Appointment(props) {
-  
-  const {id, time, interview} = props;
+  const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY)
+
+  const {id, time, interview, interviewers} = props;
+  // console.log("Appointment props:", props)
+
   return (
     <Fragment>
       <article className="appointment">
       <Header time = {time} />
-      {interview && <Show student = {interview.student} interviewer = {interview.interviewer} key = {id}/>}
-      {!interview && <Empty message = {"No Booked Appointments"} key = {id}/>}
+      {mode === SHOW && <Show student = {interview.student} interviewer = {interview.interviewer} key = {id}/>}
+      {mode === EMPTY && <Empty message = {"No Booked Appointments"} key = {id} onAdd = {() => transition(CREATE)}/>}
+      {mode === CREATE && <Form {...interview} interviewers = {interviewers} onCancel = {() => back()}/>}
       </article>
     </Fragment>
   )
