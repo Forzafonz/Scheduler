612b4b4db8408b0f401562c618dd5905dbc6125b
Anton <golubin1990@gmail.com>
2021-09-07 21:06:00 -0400

    Refactor to replace useState with useReducer. Update spots updater

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,17 +1,59 @@
-import {useState, useEffect} from 'react';
+import {useEffect, useReducer} from 'react';
+import { updateSpots } from 'helpers/selectors';
 import axios from "axios";
 
-export default function useApplicationData(initial) {
+const SET_DAY = "SET_DAY";
+const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+const SET_INTERVIEW = "SET_INTERVIEW";
 
-  const [state, setState] = useState({
+const reducer = function (state, action) {
+
+  const setDay = (state, action) =>{
+    return {...state, day: action.value.day}
+  }
+
+  const setApplicationData = (state,action) => {
+    const {days, appointments, interviewers} = action.value
+    return {...state, days, appointments, interviewers }
+  }
+
+  const setInterview = (state, action) => {
+    const {interview, id} = action.value
+    const appointment = {
+      ...state.appointments[id],
+      interview: interview ? { ...interview } : null
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    const days = updateSpots(state.days, appointments, id)
+    return {...state, appointments, days}
+  }
+
+  const actions = {
+
+    [SET_DAY]: setDay,
+    [SET_APPLICATION_DATA]: setApplicationData,
+    [SET_INTERVIEW]: setInterview,
+    "default": () => {
+      throw new Error() `Tried to reduce with unsupported action type: ${action.type}`}
+  }
+
+  return actions[action.type](state, action) || actions("default")
+
+}
+
+export default function useApplicationData(initial) {
+  const [state, dispatch] = useReducer(reducer, {
     day: "Monday",
     days: [],
     appointments: {},
     interviewers: {}
-  });
+  })
 
   const setDay = (day) => {
-    setState(prevState => ({...prevState, day : day}));
+    dispatch({ type : SET_DAY, value : {day : day}})
   }
 
   useEffect(() => {
@@ -23,78 +65,25 @@ export default function useApplicationData(initial) {
     .then(
       (result) => {
         const [days, appointments, interviewers] = result;
-        setState(prevState => ({...prevState, days: days.data, appointments: appointments.data, interviewers: interviewers.data }))
+        dispatch({ type: SET_APPLICATION_DATA, value : { days: days.data, appointments: appointments.data, interviewers: interviewers.data }})
       }
     )
   }, [])
 
   function bookInterview(id, interview) {
  
-    const setUpdateSlots = state.appointments[id].interview;
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
     return axios.put(`/api/appointments/${id}`, {interview})
-    .then((res) => {
-      if (!setUpdateSlots) {
-        const days = updateSpots(id, true)
-        setState(prevState => ({...prevState, appointments: appointments, days}))
-      } else {
-        setState(prevState => ({...prevState,  appointments: appointments}))
-      }
+    .then(() => {
+        dispatch({ type : SET_INTERVIEW, value : {id, interview}})
     })
   }
 
   function cancelInterview(id) {
   
-    const updatedAppointment = {
-
-      ...state.appointments[id], 
-         interview: null}
-
-    const newAppointmentsList = {
-
-      ...state.appointments, 
-      [id]: updatedAppointment}
-
-
-    const days = updateSpots(id)
     return axios.delete(`api/appointments/${id}`)
     .then(() => 
-      setState({...state,  appointments : newAppointmentsList , days}))
+        dispatch({ type : SET_INTERVIEW, value : {id, inverview : null }}))
   }
 
-  /** Function to increase/decrease number of spots based on Appointment id
-   * 
-   * @param {*} id - id of aoppointment
-   * @param {*} increase - indicates if number of spots should increase. Default to 'false'
-   * @returns {array} - return array of days with updated spots
-   */
-  function updateSpots(id, increase = false) {
-
-    const day = Math.floor(id / 5);
-    let spots = state.days[day].spots + 1;
-    
-    if (increase) {
-      spots -= 2;
-    }
-
-    const dayUpdate = {
-      ...state.days[day], 
-      spots }
-
-    const days = state.days.map((selectedDay, index) => index === day ? dayUpdate : selectedDay)
-
-    return days;
-
-  }
-
- 
-  return {state, bookInterview, cancelInterview, setState, setDay}
+  return {state, bookInterview, cancelInterview, setDay}
 }
