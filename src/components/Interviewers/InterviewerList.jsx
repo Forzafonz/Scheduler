c7332c15b01a9c7aa7da440d128e9e22fa8f51a3
Anton <golubin1990@gmail.com>
2021-09-08 14:15:01 -0400

    Add static propTypes check for interviewrsList props

--- src/components/Interviewers/InterviewerList.jsx
+++ src/components/Interviewers/InterviewerList.jsx
@@ -6,6 +6,7 @@
 import React from 'react';
 import InterviewerListItem from './InterviewerListItem';
 import './InterviewerList.scss';
+import PropTypes from 'prop-types';
 
 export default function InterviewerList(props) {
   
@@ -24,3 +25,7 @@ export default function InterviewerList(props) {
   )
 
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
