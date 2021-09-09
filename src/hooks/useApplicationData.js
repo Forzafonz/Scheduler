81080d8815d72fa8d92f47c08dccb4de1488cb57
Anton <golubin1990@gmail.com>
2021-09-08 21:56:04 -0400

    Refactor to separate reducer and state variables into a new file

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,48 +1,6 @@
 import {useEffect, useReducer} from 'react';
-import { updateSpots } from 'helpers/selectors';
+import reducer, {SET_DAY, SET_INTERVIEW, SET_APPLICATION_DATA} from 'reducers/application'
 import axios from "axios";
-
-const SET_DAY = "SET_DAY";
-const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-const SET_INTERVIEW = "SET_INTERVIEW";
-
-const reducer = function (state, action) {
-
-  const setDay = (state, action) =>{
-    return {...state, day: action.value.day}
-  }
-
-  const setApplicationData = (state,action) => {
-    const {days, appointments, interviewers} = action.value
-    return {...state, days, appointments, interviewers }
-  }
-
-  const setInterview = (state, action) => {
-    const {interview, id} = action.value
-    const appointment = {
-      ...state.appointments[id],
-      interview: interview ? { ...interview } : null
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    const days = updateSpots(state.days, appointments, id)
-    return {...state, appointments, days}
-  }
-
-  const actions = {
-
-    [SET_DAY]: setDay,
-    [SET_APPLICATION_DATA]: setApplicationData,
-    [SET_INTERVIEW]: setInterview,
-    "default": () => {
-      throw new Error() `Tried to reduce with unsupported action type: ${action.type}`}
-  }
-
-  return actions[action.type](state, action) || actions("default")
-
-}
 
 export default function useApplicationData(initial) {
   const [state, dispatch] = useReducer(reducer, {
---
+++ src/reducers/application.js
@@ -0,0 +1,45 @@
+import { updateSpots } from 'helpers/selectors';
+
+const SET_DAY = "SET_DAY";
+const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+const SET_INTERVIEW = "SET_INTERVIEW";
+
+const reducer = function (state, action) {
+
+  const setDay = (state, action) =>{
+    return {...state, day: action.value.day}
+  }
+
+  const setApplicationData = (state,action) => {
+    const {days, appointments, interviewers} = action.value
+    return {...state, days, appointments, interviewers }
+  }
+
+  const setInterview = (state, action) => {
+    const {interview, id} = action.value
+    const appointment = {
+      ...state.appointments[id],
+      interview: interview ? { ...interview } : null
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    const days = updateSpots(state.days, appointments, id)
+    return {...state, appointments, days}
+  }
+
+  const actions = {
+
+    [SET_DAY]: setDay,
+    [SET_APPLICATION_DATA]: setApplicationData,
+    [SET_INTERVIEW]: setInterview,
+    "default": () => {
+      throw new Error() `Tried to reduce with unsupported action type: ${action.type}`}
+  }
+
+  return actions[action.type](state, action) || actions("default")
+
+}
+
+export { reducer as default, SET_DAY, SET_INTERVIEW, SET_APPLICATION_DATA };
